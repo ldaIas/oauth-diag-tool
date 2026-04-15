98b742a375c265125e12942a8afe4b98fd45ba21
Sam Sovereign <5209310+ldaIas@users.noreply.github.com>
2026-04-15 13:59:24 -0600

    release and update init

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -206,6 +206,7 @@ async fn fetch_server_metadata(
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_updater::Builder::new().build())
     .invoke_handler(tauri::generate_handler![
         create_server_config,
         get_server_configs,
